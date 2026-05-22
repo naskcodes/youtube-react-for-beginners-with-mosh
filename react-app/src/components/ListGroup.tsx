055c819d1ec110b99b7515447888082be47b66f0
Roberta Nascimento <naskcodes@gmail.com>
2026-05-22 10:49:08 -0300

    Itens dinâmicos, formatação condicional

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
+    let items = ["Integra", "Alucard", "Seras", "Pip"];
+
     return (
         <>
-            <h1>ListGroup</h1>
+            <h1>List</h1>
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                {items.map((item, index) => (
+                    <li className="list-group-item" key={item} onClick={() => console.log(index, item)}>{item}</li>
+                ))}
             </ul>
         </>
     );
