a2bfabe1671ccd08c070dfae868d9a615a59771e
Roberta Nascimento <naskcodes@gmail.com>
2026-05-26 14:51:26 -0300

    Adição de Props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
+let items = ["Integra", "Alucard", "Seras", "Pip"];
+const handleSelectItem = (item: string) => {
+  console.log(item);
+}
+
 function App() {
-  return <div><ListGroup/></div>
+  return <div><ListGroup items={items} heading="Characters" onSelectItem={handleSelectItem}/></div>
 }
 
 export default App;
